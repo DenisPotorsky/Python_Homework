8c4bac7b67f5d4ecab423ddbff048276978fb6ab
Den <denis-s2@yandex.ru>
2022-11-29 10:08:01 +0500

    исправил 5 задачу

--- Homework/Workshop_2/Task_5.py
+++ Homework/Workshop_2/Task_5.py
@@ -7,8 +7,8 @@ import random
 lst = [1, 2, 3, 4, 5]
 temp = 0
 index = 0
-for i in range(0, 5):
-    index = random.randint(0, 4)
+for i in range(len(lst)):
+    index = random.randint(0, len(lst) -1)
     temp = lst[i]
     lst[i] = lst[index]
     lst[index] = temp
