c57cd32c0c3d9ada507ad4381fd037008021cf12
Den <denis-s2@yandex.ru>
2022-12-01 11:42:11 +0500

    выполнил д/з

--- Homework/Workshop_2/Task_5.py
+++ Homework/Workshop_2/Task_5.py
@@ -8,9 +8,7 @@ lst = [1, 2, 3, 4, 5]
 temp = 0
 index = 0
 for i in range(len(lst)):
-    index = random.randint(0, len(lst) -1)
-    temp = lst[i]
-    lst[i] = lst[index]
-    lst[index] = temp
+    index = random.randint(0, len(lst) - 1)
+    lst[i], lst[index] = lst[index], lst[i]
 
 print(lst)
---
+++ Homework/Workshop_3/Task_5.py
@@ -0,0 +1,23 @@
+'''
+Задайте число. Составьте список чисел Фибоначчи,
+в том числе для отрицательных индексов.
+Пример:
+- для k = 8 список будет выглядеть так:
+[-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
+'''
+
+length = int(input('Введите длину списка: '))
+
+lst1 = [0, 1]
+lst2 = [1, -1]
+# length = int(input())
+result = 0
+for i in range(1, length):
+    result = lst1[i] + lst1[i - 1]
+    lst1.append(result)
+for j in range(1, length - 1):
+    result = -(lst2[j]) + lst2[j - 1]
+    lst2.append(result)
+lst2.reverse()
+fibonacci = lst2 + lst1
+print(fibonacci)
